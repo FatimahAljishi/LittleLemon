77156506d37ecffdf5747ed632914c0fc0568531
s2716746 <s2716746@ed.ac.uk>
2026-04-09 13:35:02 +0300

    add token auth

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
@@ -17,5 +18,6 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MenuSerializer
 
 class BookingView(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
